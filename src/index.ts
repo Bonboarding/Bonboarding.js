3269054c31564c5d6f41c72b55d2badccac3d9b3
Johnny Fekete <hello@johnnyfekete.com>
2024-02-03 18:44:40 +0100

    init package

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+type User = {
+  uniqueID?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  signUpDate?: Date | string;
+};
+
+const Bonboarding = (() => {
+  let bbApiKey: string | null = null;
+
+  const init = (apiKey: string) => {
+    bbApiKey = apiKey;
+    console.log(`Initialized with API Key: ${bbApiKey}`);
+  };
+
+  const identify = (user: User) => {
+    if (!bbApiKey) {
+      console.warn('Bonboarding has not been initialized with an API key.');
+      return;
+    }
+    console.log(`Identifying with params:`, user);
+  };
+
+  return { init, identify };
+})();
+
+export default Bonboarding;
